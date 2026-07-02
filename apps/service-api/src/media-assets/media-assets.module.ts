cc558e925ca851c6750a3e2cd52bb037a384e716
YKDZ <3070799584@qq.com>
2026-07-02 02:29:21 +0000

    fix(service-api): import config module for media assets

--- apps/service-api/src/media-assets/media-assets.module.ts
+++ apps/service-api/src/media-assets/media-assets.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 
+import { ConfigModule } from "../config/config.module";
 import { MediaAssetsController } from "./media-assets.controller";
 import { MediaAssetsService } from "./media-assets.service";
 
 @Module({
+  imports: [ConfigModule],
   controllers: [MediaAssetsController],
   providers: [MediaAssetsService],
   exports: [MediaAssetsService],
